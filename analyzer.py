4295f8424230e1b3f04410e4d60861113b3b8d07
Mohamad Kaddoura <hamoodrex@gmail.com>
2022-04-06 18:16:54 +0300

    Analyzer
    Lexical analyzer

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,31 @@
+import re;
+rules = {
+    "IF_TOKEN": "^if$",
+    "WHILE_TOKEN": "^while$",
+    "FOR_TOKEN": "^for$",
+    "ID": "^[a-zA-Z]\w*$",
+    "INTEGER": "^\-?[0-9]+$",
+    "FLOAT": "^\-?[0-9]+\.[0-9]+$",
+    "RELOP": "^((>|<|=)=|(>|<))$",
+}
 
+
+symbol_table = dict()
+
+def lex(word):
+    for token,expression in rules.items():
+        regex = re.compile(expression)
+        if regex.match(word):
+            return token
+    return "ERROR"
+
+    
+def analyze():
+    file = open("input.dat",'r')
+    for line in file.readlines():
+        for lexeme in line.split(" "):
+            print("Token:",lex(lexeme))
+            print("Lexeme:",lexeme, end="\n\n")
+    file.close()
+    
+analyze()
